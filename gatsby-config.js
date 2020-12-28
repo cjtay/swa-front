0792cb1540ffe87b103385462cf4a3928887e1fe
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2020-12-28 11:00:15 +0800

    config update macbook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Singapore Women's Association`,
+    description: `A non-profit organisation`,
+    author: `Tay C J`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
