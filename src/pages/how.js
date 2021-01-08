5be5945e197a166b40d6b3562fb58dd179d90af8
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-01-08 18:52:14 +0800

    testing on what and how links

---
+++ src/pages/how.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const How = () => {
+  return (
+    <div>
+      <h1>How page</h1>
+      <Link to="/">
+        <p>back to home page</p>
+      </Link>
+    </div>
+  );
+};
+
+export default How;
