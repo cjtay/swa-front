ae68fc7d3cfac21cfa4d833f39798f54de781da2
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-01-25 12:00:46 +0800

    added yup validation

--- src/pages/forms/contact.js
+++ src/pages/forms/contact.js
@@ -3,7 +3,10 @@ import Layout from "../../components/layout/layout";
 import { Wrapper, ContentWrapper } from "../../styles/wrappers/Wrapper";
 import { SectionHead, Title, Description } from "../../styles/SectionHeaders";
 import SectionBackground from "../../components/backgrounds/SectionBackground";
+import { ButtonDark } from "../../styles/buttons/ButtonStyles";
+import { ErrorMsg, SuccessMsg } from "../../styles/StatusStyles";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import styled from "styled-components";
 
@@ -11,35 +14,62 @@ const ContactForm = () => {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      phone: "",
-      message: "",
-    },
+  const initialValues = {
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
-    onSubmit: async values => {
-      console.log("submitted data: ", values);
-      try {
-        const response = await fetch("http://localhost:1337/contact-forms", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(values),
-        });
-        const data = await response.json();
-        console.log("Successfully sent to Strapi!");
-        console.log("response received: ", data);
-        if (data.statusCode) {
-          setError(data.message);
-        } else if (data.created_at) {
-          setSuccess(true);
-        }
-      } catch (error) {
-        console.log("error received: ", error.message);
+  const onSubmit = async values => {
+    console.log("submitted data: ", values);
+    try {
+      const response = await fetch("http://localhost:1337/contact-forms", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      console.log("Successfully sent to Strapi!");
+      console.log("response received: ", data);
+      if (data.statusCode) {
+        setError(data.message);
+      } else if (data.created_at) {
+        setSuccess(true);
       }
-      // .catch(error => console.log("error received: ", error));
-    },
+    } catch (error) {
+      console.log("error received: ", error.message);
+    }
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Please provide your name"),
+    email: Yup.string()
+      .email("Please enter valid email")
+      .required("Email address is required"),
+    message: Yup.string().required("Please enter your message"),
+  });
+
+  // const validate = values => {
+  //   let errors = {};
+  //   if (!values.name) {
+  //     errors.name = "Please provide your name";
+  //   }
+  //   if (!values.email) {
+  //     errors.email = "Email address is required";
+  //   }
+  //   if (!values.message) {
+  //     errors.message = "Please enter your message";
+  //   }
+
+  //   return errors;
+  // };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    // validate,
+    validationSchema,
   });
 
   console.log("formik: ", formik);
@@ -70,8 +100,12 @@ const ContactForm = () => {
                 id="name"
                 name="name"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.name}
               />
+              {formik.touched.name && formik.errors.name && (
+                <ErrorMsg>{formik.errors.name}</ErrorMsg>
+              )}
             </FormGroup>
             <FormGroup>
               <label htmlFor="email">Email</label>
@@ -80,8 +114,12 @@ const ContactForm = () => {
                 id="email"
                 name="email"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.email}
               />
+              {formik.touched.email && formik.errors.email && (
+                <ErrorMsg>{formik.errors.email}</ErrorMsg>
+              )}
             </FormGroup>
             <FormGroup>
               <label htmlFor="phone">Phone</label>
@@ -90,21 +128,31 @@ const ContactForm = () => {
                 id="phone"
                 name="phone"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.phone}
                 placeholder="optional"
               />
+              {formik.touched.phone && formik.errors.phone && (
+                <ErrorMsg>{formik.errors.ErrorMsghone}</ErrorMsg>
+              )}
             </FormGroup>
             <FormGroup>
               <label htmlFor="message">Message</label>
               <textarea
                 name="message"
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 value={formik.values.message}
               />
+              {formik.touched.message && formik.errors.message && (
+                <ErrorMsg>{formik.errors.message}</ErrorMsg>
+              )}
             </FormGroup>
-            <button type="submit">Submit</button>
-            {error !== "" && <p>Error encountered: {error}</p>}
-            {success && <p>Submitted successfully</p>}
+            <ButtonDark center onClick={formik.handleSubmit}>
+              Submit
+            </ButtonDark>
+            {error !== "" && <ErrorMsg>Error encountered: {error}</ErrorMsg>}
+            {success && <SuccessMsg>Submitted successfully</SuccessMsg>}
           </FormContainer>
         </ContentWrapper>
       </Wrapper>
@@ -141,7 +189,8 @@ const FormGroup = styled.div`
     font-weight: 700;
   }
 
-  input {
+  input,
+  textarea {
     padding: 0.5em;
     font-size: 1rem;
     color: grey;
---
+++ src/styles/StatusStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ErrorMsg = styled.p`
+  color: var(--color-error);
+  font-size: 1rem;
+  margin-top: 0.2em;
+`;
+
+export const SuccessMsg = styled.p`
+  color: var(--color-success);
+  /* font-weight: 700; */
+  /* font-size: 1rem; */
+`;
