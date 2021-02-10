65de60948711cd6eee51f7221d3b37d23471bbce
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-02-10 13:16:59 +0800

    updated font family config

--- src/pages/forms/contact-hook.js
+++ src/pages/forms/contact-hook.js
@@ -48,7 +48,7 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="px-4 my-12 max-w-3xl mx-auto">
+    <div className="px-4 my-12 max-w-3xl mx-auto antialiased text-gray-800">
       <h1 className="text-4xl font-black">Enquiry Form</h1>
       <p className="text-gray-500">We love to hear from you.</p>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      body: ["Roboto", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["Inter"],
+      },
     },
-    extend: {},
     variants: {
       extend: {},
     },
