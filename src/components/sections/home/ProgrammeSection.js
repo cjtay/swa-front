c72e6a8af8bf5ef61c999db7e393b9977b7a008c
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-02-21 15:31:45 +0800

    program section tailwind complete

--- src/components/sections/home/ProgrammeSection.js
+++ src/components/sections/home/ProgrammeSection.js
@@ -1,140 +1,88 @@
 import React from "react";
-import styled from "styled-components";
-import { ButtonDark } from "../../../styles/buttons/ButtonStyles";
-import { Wrapper, ContentWrapper } from "../../../styles/wrappers/Wrapper";
-import { Header, Title, Description } from "../../../styles/SectionHeaders";
 
 const ProgrammeSection = () => {
   return (
-    <Wrapper>
-      <ContentWrapper>
-        <Header>
-          <Title>Key Programmes</Title>
-          <Description>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            fugiat hic eaque nobis modi laborum ipsam unde laboriosam earum
-            explicabo, nostrum excepturi rem totam labore odio deleniti
-            cupiditate cum facilis?
-          </Description>
-          <ButtonPosition>
-            <ButtonDark>View programmes</ButtonDark>
-          </ButtonPosition>
-        </Header>
-        <ProgrammeList>
-          <ProgrammeCard>
-            <ProgrammeText>
-              <ProgrammeTitle>Elderly</ProgrammeTitle>
-              <Description>programme description</Description>
-            </ProgrammeText>
-            <ProgrammeImage>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </ProgrammeImage>
-          </ProgrammeCard>
-          <ProgrammeCard>
-            <ProgrammeText>
-              <ProgrammeTitle>Community</ProgrammeTitle>
-              <Description>programme description</Description>
-            </ProgrammeText>
-            <ProgrammeImage>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </ProgrammeImage>
-          </ProgrammeCard>
-          <ProgrammeCard>
-            <ProgrammeText>
-              <ProgrammeTitle>Diversity</ProgrammeTitle>
-              <Description>programme description</Description>
-            </ProgrammeText>
-            <ProgrammeImage>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </ProgrammeImage>
-          </ProgrammeCard>
-          <ProgrammeCard>
-            <ProgrammeText>
-              <ProgrammeTitle>Leadership</ProgrammeTitle>
-              <Description>programme description</Description>
-            </ProgrammeText>
-            <ProgrammeImage>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </ProgrammeImage>
-          </ProgrammeCard>
-        </ProgrammeList>
-      </ContentWrapper>
-    </Wrapper>
+    <section className="sm:w-4/5 max-w-4xl px-2 mx-auto">
+      <div className="sm:w-3/5">
+        <h2 className="text-purple-900">Key Programmes</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          fugiat hic eaque nobis modi laborum ipsam unde laboriosam earum
+          explicabo, nostrum excepturi rem totam labore odio deleniti cupiditate
+          cum facilis?
+        </p>
+        <div className="flex flex-row justify-center sm:justify-start ">
+          <div className="btn-dark my-4">View programmes</div>
+        </div>
+      </div>
+      <div className="sm:max-w-xl mr-auto">
+        <div className="sm:flex sm:flex-row-reverse p-4 bg-gradient-to-br from-swa-5 via-swa-5 to-swa-1 rounded-xl my-5">
+          <div className="text-white">
+            <h4>Elderly</h4>
+            <p className="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut
+              mauris at felis faucibus consectetur. Proin enim lacus, sagittis
+              vel turpis in, vehicula venenatis arcu. Sed ac nisl nec nisi
+              ultrices convallis.
+            </p>
+          </div>
+          <img
+            src="https://source.unsplash.com/random"
+            alt=""
+            className="max-h-full border-4 border-white sm:mb-2 mt-2 sm:mt-0 sm:mr-4 sm:w-40 sm:h-40 object-cover"
+          />
+        </div>
+        <div className="sm:flex sm:flex-row-reverse p-4 bg-gradient-to-br from-swa-5 via-swa-5 to-swa-1 rounded-xl my-5">
+          <div className="text-white">
+            <h4>Community</h4>
+            <p className="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut
+              mauris at felis faucibus consectetur. Proin enim lacus, sagittis
+              vel turpis in, vehicula venenatis arcu. Sed ac nisl nec nisi
+              ultrices convallis.
+            </p>
+          </div>
+          <img
+            src="https://source.unsplash.com/random"
+            alt=""
+            className="max-h-full border-4 border-white sm:mb-2 mt-2 sm:mt-0 sm:mr-4 sm:w-40 sm:h-40 object-cover"
+          />
+        </div>
+        <div className="sm:flex sm:flex-row-reverse p-4 bg-gradient-to-br from-swa-5 via-swa-5 to-swa-1 rounded-xl my-5">
+          <div className="text-white">
+            <h4>Diversity</h4>
+            <p className="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut
+              mauris at felis faucibus consectetur. Proin enim lacus, sagittis
+              vel turpis in, vehicula venenatis arcu. Sed ac nisl nec nisi
+              ultrices convallis.
+            </p>
+          </div>
+          <img
+            src="https://source.unsplash.com/random"
+            alt=""
+            className="max-h-full border-4 border-white sm:mb-2 mt-2 sm:mt-0 sm:mr-4 sm:w-40 sm:h-40 object-cover"
+          />
+        </div>
+        <div className="sm:flex sm:flex-row-reverse p-4 bg-gradient-to-br from-swa-5 via-swa-5 to-swa-1 rounded-xl my-5">
+          <div className="text-white">
+            <h4>Community</h4>
+            <p className="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut
+              mauris at felis faucibus consectetur. Proin enim lacus, sagittis
+              vel turpis in, vehicula venenatis arcu. Sed ac nisl nec nisi
+              ultrices convallis.
+            </p>
+          </div>
+          <img
+            src="https://source.unsplash.com/random"
+            alt=""
+            className="max-h-full border-4 border-white sm:mb-2 mt-2 sm:mt-0 sm:mr-4 sm:w-40 sm:h-40 object-cover"
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
 export default ProgrammeSection;
-
-const ButtonPosition = styled.div`
-  align-self: center;
-  margin-bottom: 1.5em;
-
-  @media (min-width: 600px) {
-    align-self: flex-start;
-  }
-`;
-
-const ProgrammeList = styled.ul`
-  @media (min-width: 600px) {
-    display: flex;
-    margin: 0 auto;
-  }
-`;
-
-const ProgrammeCard = styled.li`
-  background: rgba(112, 49, 140, 1);
-  background: -webkit-linear-gradient(
-    bottom right,
-    rgba(112, 49, 140, 1),
-    rgba(132, 137, 199, 1)
-  );
-  background: -moz-linear-gradient(
-    bottom right,
-    rgba(112, 49, 140, 1),
-    rgba(132, 137, 199, 1)
-  );
-  background: linear-gradient(
-    to top left,
-    rgba(112, 49, 140, 1),
-    rgba(132, 137, 199, 1)
-  );
-  padding: 1em;
-  border-radius: 10px;
-  margin-top: 0.5em;
-
-  @media (min-width: 600px) {
-    margin-right: 0.5em;
-    display: flex;
-    flex-direction: column-reverse;
-
-    :last-child {
-      margin-right: 0;
-    }
-  }
-`;
-
-const ProgrammeText = styled.div`
-  color: var(--color-white);
-`;
-
-const ProgrammeTitle = styled.h4`
-  margin-bottom: 0.2em;
-  /* text-align: center; */
-  font-size: rem;
-`;
-
-const ProgrammeImage = styled.div`
-  max-height: 200px;
-  overflow: hidden;
-  border: 5px solid white;
-  margin-top: 0.5em;
-
-  img {
-    display: block;
-    max-width: 100%;
-    max-height: auto;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: var(--color-white);
-  }
-`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       colors: {
         "white-transparent": "rgba(255, 255, 255, 0.75)",
         "black-transparent": "rgba(0, 0, 0, 0.2)",
+        "swa-1": "rgb(112, 48, 140)",
+        "swa-2": "rgb(69, 10, 94)",
+        "swa-3": "rgb(135, 75, 161)",
+        "swa-4": "rgb(243, 210, 255)",
+        "swa-5": "rgba(132, 137, 199, 1)",
       },
     },
     variants: {
