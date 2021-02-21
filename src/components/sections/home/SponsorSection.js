70146080d7c1111e6790d4ba75c349d7102d8f62
Tay CJ <cjtay@Tays-MacBook-Pro.local>
2021-02-21 17:27:53 +0800

    sponsor tailwind complete

--- src/components/sections/home/SponsorSection.js
+++ src/components/sections/home/SponsorSection.js
@@ -1,7 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-import { Wrapper, ContentWrapper } from "../../../styles/wrappers/Wrapper";
-import { Title, Description } from "../../../styles/SectionHeaders";
 
 import crystaltime from "../../../images/crystaltime.png";
 import florray from "../../../images/florray.png";
@@ -12,77 +9,56 @@ import yvonne from "../../../images/yvonne.png";
 
 const SponsorSection = () => {
   return (
-    <>
-      <Wrapper>
-        <ContentWrapper>
-          <Title>Sponsors</Title>
-          <Description>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-            accusamus fugiat expedita modi eveniet maxime aperiam excepturi
-            repellendus similique! Molestiae sequi ex minima labore culpa
-            pariatur dicta omnis blanditiis enim.
-          </Description>
+    <section className="sm:w-4/5 max-w-4xl px-2 mx-auto">
+      <h2 className="text-purple-900">Sponsors</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum accusamus
+        fugiat expedita modi eveniet maxime aperiam excepturi repellendus
+        similique! Molestiae sequi ex minima labore culpa pariatur dicta omnis
+        blanditiis enim.
+      </p>
 
-          <List>
-            <Item>
-              <Logo src={crystaltime} alt="crystal time logo" />
-            </Item>
-            <Item>
-              <Logo src={florray} alt="crystal time logo" />
-            </Item>
-            <Item>
-              <Logo src={moephosis} alt="crystal time logo" />
-            </Item>
-            <Item>
-              <Logo src={teamsalon} alt="crystal time logo" />
-            </Item>
-            <Item>
-              <Logo src={yvonne} alt="crystal time logo" />
-              {/* <img
-              src="/images/yvonne.png"
-              alt="yvonne creative logo"
-              class="sponsor-logo"
-            /> */}
-            </Item>
-          </List>
-        </ContentWrapper>
-      </Wrapper>
+      <div className="p-4 flex flex-col sm:flex-row sm:flex-wrap justify-center items-center sm:max-w-3xl mx-auto ">
+        <div>
+          <img
+            src={crystaltime}
+            alt="crystal time logo"
+            className="w-4/5 sm:w-48 m-3 p-2 border border-gray-200"
+          />
+        </div>
+        <div>
+          <img
+            src={florray}
+            alt="crystal time logo"
+            className="w-4/5 sm:w-48 m-3 border border-gray-200"
+          />
+        </div>
+        <div>
+          <img
+            src={moephosis}
+            alt="crystal time logo"
+            className="w-4/5 sm:w-48 m-3 border border-gray-200"
+          />
+        </div>
+        <div>
+          <img
+            src={teamsalon}
+            alt="crystal time logo"
+            className="w-4/5 sm:w-48 m-3 border border-gray-200"
+          />
+        </div>
+        <div>
+          <img
+            src={yvonne}
+            alt="crystal time logo"
+            className="w-4/5 sm:w-48 m-3 border border-gray-200"
+          />
+        </div>
+      </div>
+
       {/* <SponsorBackground /> */}
-    </>
+    </section>
   );
 };
 
 export default SponsorSection;
-
-const List = styled.ul`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-color: var(--color-white-transparent);
-  border-radius: 10px;
-  margin: 1em auto;
-  /* border: 1px solid red; */
-
-  @media (min-width: 600px) {
-    flex-direction: row;
-    flex-wrap: wrap;
-    max-width: 800px;
-  }
-`;
-
-const Item = styled.li`
-  align-self: center;
-  padding-top: 1em;
-  /* width: 100%; */
-  :last-child {
-    padding-bottom: 1em;
-  }
-  @media (min-width: 600px) {
-    margin-left: 1.5em;
-  }
-`;
-
-const Logo = styled.img`
-  max-width: 180px;
-  border: 1px solid var(--color-lightgrey);
-`;
