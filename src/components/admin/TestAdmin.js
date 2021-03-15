327e7817d2f263579923a65811fe831cebdfd63a
Tay CJ <cjtay@outlook.sg>
2021-03-15 18:48:11 +0800

    setup admin page

---
+++ src/components/admin/TestAdmin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestAdmin = () => {
+  return (
+    <div>
+      <h2 className="text-red-500">This is a admin component</h2>
+    </div>
+  );
+};
+
+export default TestAdmin;
---
+++ src/pages/swaadmin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TestAdmin from "../components/admin/TestAdmin";
+
+const SwaAdmin = () => {
+  return (
+    <div>
+      <h1>SWA Admin</h1>
+      <TestAdmin />
+    </div>
+  );
+};
+
+export default SwaAdmin;
